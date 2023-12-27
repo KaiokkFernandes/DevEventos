6185500f47ba24bc6b62b3ed408f397aeef0d273
KaiokkFernandes <kaiofernandess654@gmail.com>
2023-12-27 16:03:53 -0300

    criando Projeto Api e adicionando os https

---
+++ Back/src/Controllers/EventoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace src.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        
+
+       
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            return "Value Get";
+        }
+
+        [HttpPut("{id}")]
+        public String Put(int id)
+        {
+            return "Value Put";
+        }
+
+        [HttpDelete("{id}")]
+        public String Delete(){
+            return "Value Delete";
+        }
+       }
+    }
+
+    
